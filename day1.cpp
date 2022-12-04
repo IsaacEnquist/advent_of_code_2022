878f719492cc563acde656a1f6aabf93c7454ce4
IsaacEnquist <isaac.enquist@gmail.com>
2022-12-04 10:40:32 +0100

    day 1

--- day1.cpp
+++ day1.cpp
@@ -1,21 +1,44 @@
-#include <iostream>
+#include <algorithm>
 #include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
 #include <string>
 using namespace std;
+
 int main(){
    fstream newfile;
-   newfile.open("day1.txt",ios::out);  // open a file to perform write operation using file object
-   if(newfile.is_open()) //checking whether the file is open 
-   {
-      newfile<<"Tutorials point "; //inserting text
-      newfile.close(); //close the file object
-   }
+   string line;
+   int max {0};
+   int max2 {0};
+   int max3 {0};
+   int current {0};
+   int temp {0};
    newfile.open("day1.txt",ios::in); //open a file to perform read operation using file object
-   if (newfile.is_open()){ //checking whether the file is open
-      string tp;
-      while(getline(newfile, tp)){ //read data from file object and put it into string.
-         cout << tp << ""; //print the data of the string
-      }
-      newfile.close(); //close the file object.
+    while(getline(newfile, line)){
+        if(line.compare("") == 0){
+            if(current > max){
+                temp = max;
+                max = current;
+                current = temp;
+                if (current > max2){
+                temp = max2;
+                max2 = current;
+                current = temp;
+                    if (current > max3){
+                    temp = max3;
+                    max3 = current;
+                    current = temp;
+                    }
+                }
+            }
+            current = 0;
+        } else
+        current = current + stoi(line);
+        }
+        cout << max << endl;
+        cout << max2 << endl;
+        cout << max3 << endl;
+        cout << max+max2+max3 << endl;
    }
-}
+
