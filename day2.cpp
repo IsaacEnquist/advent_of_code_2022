549241ebc0b374a569c128725571e68d9dee703f
IsaacEnquist <isaac.enquist@gmail.com>
2022-12-04 11:02:32 +0100

    day2 part 1

---
+++ day2.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main(){
+   fstream newfile;
+   string line;
+   string me;
+   string elf;
+   int score {0};
+   newfile.open("day2.txt",ios::in); //open a file to perform read operation using file object
+    while(getline(newfile, line)){
+        elf = line[0];
+        me = line[2];
+        if(me.compare("X") == 0){
+            if(elf.compare("A")==0){
+                score = score + 3;
+            }
+            else if(elf.compare("C")==0){
+                score = score + 6;
+            }
+            score = score + 1;
+        }
+        else if(me.compare("Y") == 0){
+            if(elf.compare("A")==0){
+                score = score + 6;
+            }
+            else if(elf.compare("B")==0){
+                score = score + 3;
+            }
+            score = score + 2;
+        }
+        else{
+            if(elf.compare("C")==0){
+                score = score + 3;
+            }
+            else if(elf.compare("B")==0){
+                score = score + 6;
+            }
+            score = score + 3;
+        }
+    }
+    cout << score;
+}
