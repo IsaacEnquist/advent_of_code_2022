4e98450bfb0d6f4d25ed745460b47cba2a0a5dca
IsaacEnquist <isaac.enquist@gmail.com>
2022-12-04 11:11:13 +0100

    day2 part 2

--- day2.cpp
+++ day2.cpp
@@ -21,27 +21,36 @@ int main(){
                 score = score + 3;
             }
             else if(elf.compare("C")==0){
-                score = score + 6;
+                score = score + 2;
             }
-            score = score + 1;
+            else{
+                score = score + 1;
+            }
+            score = score + 0;
         }
         else if(me.compare("Y") == 0){
             if(elf.compare("A")==0){
-                score = score + 6;
+                score = score + 1;
             }
             else if(elf.compare("B")==0){
+                score = score + 2;
+            }
+            else{
                 score = score + 3;
             }
-            score = score + 2;
+            score = score + 3;
         }
         else{
             if(elf.compare("C")==0){
-                score = score + 3;
+                score = score + 1;
             }
             else if(elf.compare("B")==0){
-                score = score + 6;
+                score = score + 3;
             }
-            score = score + 3;
+            else{
+                score = score + 2;
+            }
+            score = score + 6;
         }
     }
     cout << score;
