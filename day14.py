7344411afd9b732fcdaf52a23b0ec4466050babe
isaacenquist <isaac.enquist@gmail.com>
2022-12-14 10:49:53 +0100

    day 14 part 2

--- day14.py
+++ day14.py
@@ -5,8 +5,8 @@ import numpy as np
 input = open('day14.txt', 'r')
 input = input.readlines()
 
-begin = 400
-bottom = 200
+begin = 200
+bottom = 170
 
 cave = np.zeros((bottom + 1, (500-begin)*2))
 
@@ -18,23 +18,28 @@ for i in range(len(input)):
 
 rocks = [eval(input[i]) for i in range(len(input))]
 
+maxy = 0
 for i in range(len(rocks)):
     for j in range(int(len(rocks[i])/2)-1):
         if rocks[i][j*2] != rocks[i][j*2+2]:
             for k in range(abs(rocks[i][j*2] - rocks[i][j*2+2])+1):
                 cave[rocks[i][j*2+1]][min(rocks[i][j*2], rocks[i][j*2+2]) + k - begin] = 1
+                maxy = max(maxy, rocks[i][j*2+1])
         else:
             for k in range(abs(rocks[i][j*2+1] - rocks[i][j*2+3])+1):
                 cave[min(rocks[i][j*2+1], rocks[i][j*2+3])+k][rocks[i][j*2]-begin] = 1
+                maxy = max(rocks[i][j*2+1], maxy, rocks[i][j*2+3])
 
-print(cave)
+cave[maxy + 2][:] = 1
   
-abyss = False
+
 units = 0
-while not abyss:
+while True:
     x = 500
     y = 0
     units += 1
+    if cave[0][500-begin] == 1:
+        break
     while True:
         if cave[y+1][x-begin] == 0:
             y += 1
@@ -47,9 +52,5 @@ while not abyss:
         else:
             cave[y][x-begin] = 1
             break
-        if y >= bottom:
-            abyss = True
-            break
-    #print(cave)
 
 print(units - 1)      
